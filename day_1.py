d2df74c0dd46ec495329d569dc017bc8a65464c8
cecelia-byte <rsamridhi2001@gmail.com>
2026-05-30 11:11:10 +0530

    daily python progress

---
+++ day_1.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+print("hi i am learning python\nthis is a new line")
+# and this is a comment
+print("hey i am a \'good\' girl\nand this is a new line")
+'''this is multi line comment
+day 1 of python'''
+
+print("hey",6,7,sep='~',end='69\n')
