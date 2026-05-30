1f719426ffb3795fdf3f1d7974fa50e026e919df
cecelia-byte <rsamridhi2001@gmail.com>
2026-05-30 13:38:40 +0530

    updated strings

--- strings.py
+++ strings.py
@@ -1,5 +1,55 @@
-str1="hello"
-str2="git"
-str3=str1+str2
-print(str3)
-print(len(str3))
+# str1="hello"
+# str2="git"
+# str3=str1+str2
+# print(str3)
+# print(len(str3))
+# str="Samridhi"
+# char=str[0]
+# print(char)
+# print(str[0:4])
+# ztr="hi this lecture sucks"
+# print(ztr.find("sucks"))
+# p=ztr.count("o")
+# print(p)
+# i=ztr.index("sucks")
+# print(i)
+# k=ztr.index("y")
+# print(k)
+# input users name and print its length
+# name=input("enter your name")
+# print(len(name))
+# # find occurence of $ in a string
+# string=input("enter a string")
+# print(string.find("$"))
+
+# # if elif else
+# age=int(input("enter your age: "))
+# if(age>=18):
+#     print("you can vote")
+# elif(age<18 and age>=0):
+#     print("you cannot vote")
+# else: 
+#     print("bruh what")
+
+# to check if a number is even or odd
+# num=int(input("enter a number: "))
+# if(num%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# # to find the largest of three numbers
+# num1=float(input("enter first number: "))
+# num2=float(input("enter second number: "))
+# num3=float(input("enter third number: "))
+# if(num1>num2 and num1>num3):
+#     print("largest is",num1)
+# elif(num2>num1 and num2>num3):
+#     print("largest is",num2)
+# else:    print("largest is",num3)   
+
+# to check if a number is a multiple of 7 or not
+num=int(input("enter a number: "))
+if(num%7==0):
+    print("multiple of 7")
+else:    print("not a multiple of 7")
