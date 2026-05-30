6628d2227e79f4865f9c2c7b2a39f279dbb943c2
cecelia-byte <rsamridhi2001@gmail.com>
2026-05-30 12:20:42 +0530

    day_2.py

---
+++ day_2.py
@@ -0,0 +1,35 @@
+# print sum
+a=34
+b=584
+print(a+b)
+# relational operator
+print(a>b)
+# logical operator
+print(a>10 and b>500)
+print(a<10 and b>500)#false and true is false
+# assignment operator
+a=a+1
+print(a)
+a+=1
+print(a)
+# type conversion
+c=int("37")
+d=float("3.14")
+print(c+d)
+# input function
+# name=input("enter your name: ")
+# print("hello",name)
+# print(type(name))
+# age=int(input("enter your age: "))
+# print(type(age))
+# #input two numbers and print their sum
+# num1=float(input("enter first number: "))
+# num2=float(input("enter second number: "))
+# print("the sum is:",num1+num2)    
+# print area of square
+# side=float(input("enter the side of square: "))
+# area=side**2
+# print("the area of square is:",area)
+x=int(input("enter a number: "))
+y=int(input("enter another number: "))
+print(x>=y)
